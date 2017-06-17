bf4321e2334061890b7111718901f79a4b5ed2b0
Justin Carter <justin@starkandwayne.com>
2017-06-17 21:53:35 +0200

    Supporting habitat plans

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # habitat-plans
-Plans for habitat.sh.
+
+This repository contains habitat packages that are used as dependencies to the habitat packaged services under github.com/misthosio/tezos-habitat. Since they are mostly supporting libraries they should probably be in habitat/core-plans and we will try to get them accepted there.
+
+All the packages have been uploaded to the habitat public depot and can be downloaded via: `hab pkg install misthosio/opam`
---
+++ snappy/plan.sh
@@ -0,0 +1,26 @@
+pkg_origin=misthosio
+pkg_name=snappy
+pkg_version=1.1.4
+pkg_maintainer="The Misthos.io team <github@misthos.io>"
+pkg_license=('BSD-3-Clause')
+pkg_source=https://github.com/google/snappy/releases/download/${pkg_version}/snappy-${pkg_version}.tar.gz
+pkg_shasum=134bfe122fd25599bb807bb8130e7ba6d9bdb851e0b16efcb83ac4f5d0b70057
+pkg_deps=(core/glibc)
+pkg_build_deps=(core/gcc core/make core/autoconf core/automake core/pkg-config core/libtool core/m4 core/sed core/pkg-config)
+pkg_bin_dirs=(bin)
+pkg_include_dirs=(include)
+pkg_lib_dirs=(lib)
+pkg_upstream_url=https://github.com/google/snappy
+pkg_description="A fast compressor/decompressor http://google.github.io/snappy/"
+
+do_build () {
+  libtoolize --force
+  ACLOCAL_PATH=$(pkg_path_for core/pkg-config)/share/aclocal autoreconf -iv
+  ./configure --prefix="$pkg_prefix"
+  make
+}
+
+do_check () {
+  make check
+}
+
